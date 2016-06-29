e2b8d44440e7994f67847ff1a2b135b0e42001b5
Roland Angerer <r.angerer@yelsterdigital.com>
2016-06-29 19:37:30 +0200

    first round

--- roman.py
+++ roman.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def roman():
-    pass
+import math
+
+numerals = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M',
+}
+
+def roman(number):
+    result = ""
+    if number == 0:
+        return result
+    while(number > 0):
+        for key in sorted(numerals, reverse=True):
+            if number >= key:
+                x = int(math.floor(number / key))
+                if x == 4 or x == 90:
+                    result += specials[key * x]
+                else:
+                    result = result + x * numerals[key]
+                number = number - key * x
+    return result
 
 if __name__ == "__main__":
     import nose
--- test_roman.py
+++ test_roman.py
@@ -5,5 +5,53 @@ from nose.tools import *
 
 from roman import roman
 
-def test_roman():
-    pass
+def test_roman_1():
+    assert_equals(roman(1), 'I')
+    
+def test_roman_5():
+    assert_equals(roman(5), 'V')
+
+def test_roman_10():
+    assert_equals(roman(10), 'X')
+    
+def test_roman_50():
+    assert_equals(roman(50), 'L')
+    
+def test_roman_110():
+    assert_equals(roman(110), 'CX')
+    
+def test_roman_0():
+    assert_equals(roman(0), "")
+    
+def test_roman_1510():
+    assert_equals(roman(1510), "MDX")
+    
+def test_roman_4():
+    assert_equals(roman(4), "IV")
+    
+def test_roman_9():
+    assert_equals(roman(9), "IX")
+    
+def test_roman_44():
+    assert_equals(roman(44), 'XLIV')
+    
+def test_roman_99():
+    assert_equals(roman(99), 'XCIX')
+
+def test_roman_49():
+    assert_equals(roman(49), 'XLIX')
+
+def test_roman_34():
+    assert_equals(roman(34), 'XXXIV')
+
+def test_roman_39():
+    assert_equals(roman(39), 'XXXIX')
+    
+def test_roman_1999():
+    assert_equals(roman(1999), 'MCMXCIX')
+
+def test_roman_1499():
+    assert_equals(roman(1499), 'MCDXCIX')
+
+
+
