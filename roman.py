01ef911e79e7f838034c27a6e26905fa195503bb
Roland Angerer <r.angerer@yelsterdigital.com>
2016-06-29 13:47:12 +0200

    initial commit

---
+++ roman.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def roman():
+    pass
+
+if __name__ == "__main__":
+    import nose
+    nose.main()
---
+++ test_roman.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import *
+
+from roman import roman
+
+def test_roman():
+    pass
